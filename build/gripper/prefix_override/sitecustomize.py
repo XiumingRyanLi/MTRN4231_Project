cae7a46d64a6cb225f791fcadc3d1256998f1703
Justin Kwok <103405185+QwertyYouJK@users.noreply.github.com>
2025-10-28 15:56:49 +1100

    Put chess master packages in src

--- build/gripper/prefix_override/sitecustomize.py
+++ build/gripper/prefix_override/sitecustomize.py
@@ -1,4 +1,4 @@
 import sys
 if sys.prefix == '/usr':
     sys.real_prefix = sys.prefix
-    sys.prefix = sys.exec_prefix = '/home/ryanl/4231/MTRN4231_Project/install/gripper'
+    sys.prefix = sys.exec_prefix = '/root/MTRN4231_Project/install/gripper'
--- install/gripper/share/gripper/package.ps1
+++ install/gripper/share/gripper/package.ps1
@@ -112,6 +112,5 @@ $env:COLCON_CURRENT_PREFIX=(Get-Item $PSCommandPath).Directory.Parent.Parent.Ful
 
 colcon_package_source_powershell_script "$env:COLCON_CURRENT_PREFIX\share/gripper/hook/pythonpath.ps1"
 colcon_package_source_powershell_script "$env:COLCON_CURRENT_PREFIX\share/gripper/hook/ament_prefix_path.ps1"
-colcon_package_source_powershell_script "$env:COLCON_CURRENT_PREFIX\../../build/gripper/share/gripper/hook/pythonpath_develop.ps1"
 
 Remove-Item Env:\COLCON_CURRENT_PREFIX
