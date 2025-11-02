b2398c44e1cb8be162967b8c2a50ff44d8ac5c72
Ryan Li <z5422416@ad.unsw.edu.au>
2025-11-02 16:58:32 +1100

    can visualise endeffector

--- src/arm_controller/launch/view_combined.launch.py
+++ src/arm_controller/launch/view_combined.launch.py
@@ -1,65 +1,38 @@
 import os
+
+from ament_index_python.packages import get_package_share_directory
 from launch import LaunchDescription
 from launch_ros.actions import Node
-from launch.substitutions import PathJoinSubstitution, Command, FindExecutable
-from launch_ros.substitutions import FindPackageShare
-from launch_ros.parameter_descriptions import ParameterValue
+import xacro
 
 def generate_launch_description():
-    # same xacro you use in arm_controller.launch.py
-    wrapper_xacro = PathJoinSubstitution(
-        [FindPackageShare("arm_controller"), "urdf", "ur5e_with_tool.urdf.xacro"]
-    )
-    joint_limit_params = PathJoinSubstitution(
-        [FindPackageShare("ur_description"), "config", "ur5e", "joint_limits.yaml"]
-    )
-    kinematics_params = PathJoinSubstitution(
-        [FindPackageShare("ur_description"), "config", "ur5e", "default_kinematics.yaml"]
-    )
-    physical_params = PathJoinSubstitution(
-        [FindPackageShare("ur_description"), "config", "ur5e", "physical_parameters.yaml"]
-    )
-    visual_params = PathJoinSubstitution(
-        [FindPackageShare("ur_description"), "config", "ur5e", "visual_parameters.yaml"]
-    )
 
-    robot_description_content = Command([
-        PathJoinSubstitution([FindExecutable(name="xacro")]), " ",
-        wrapper_xacro, " ",
-        "joint_limit_params:=", joint_limit_params, " ",
-        "kinematics_params:=",  kinematics_params,  " ",
-        "physical_params:=",    physical_params,    " ",
-        "visual_params:=",      visual_params,      " ",
-        "safety_limits:=",      "true",             " ",
-        "safety_pos_margin:=",  "0.15",             " ",
-        "safety_k_position:=",  "20",               " ",
-        "name:=",               "ur",               " ",
-        "ur_type:=",            "ur5e",             " ",
-        "prefix:=",             '""',               " ",
-    ])
-    robot_description = {
-        "robot_description": ParameterValue(robot_description_content, value_type=str)
-    }
+    package_name = 'arm_controller'
+    xacro_path = 'urdf/ur5e_with_tool.urdf.xacro'
+    rviz_path = 'rviz/display.rviz'
 
-    rviz_config = PathJoinSubstitution(
-        [FindPackageShare("arm_controller"), "rviz", "display.rviz"]
-    )
+    xacro_file = os.path.join(get_package_share_directory(package_name), xacro_path)
+    xacro_raw_description = xacro.process_file(xacro_file).toxml()
+
+    rviz_file = os.path.join(get_package_share_directory(package_name), rviz_path)
 
     return LaunchDescription([
-        # publish TF for the combined model
         Node(
-            package="robot_state_publisher",
-            executable="robot_state_publisher",
-            name="combined_robot_state_publisher",
-            parameters=[robot_description],
-            output="screen",
-        ),
-        # RViz
+            name='robot_state_publisher',
+            package='robot_state_publisher',
+            executable='robot_state_publisher',
+            output='screen',
+            parameters=[{'robot_description': xacro_raw_description}]),
         Node(
-            package="rviz2",
-            executable="rviz2",
-            name="combined_rviz",
-            arguments=["-d", rviz_config],
-            output="screen",
-        ),
+            name='joint_state_publisher_gui',
+            package='joint_state_publisher_gui',
+            executable='joint_state_publisher_gui',
+            output='screen',
+            parameters=[{'robot_description': xacro_raw_description, 'source_list': ['cutting_joint']}]),
+        Node(
+            name='rviz2',
+            package='rviz2',
+            executable='rviz2',
+            output='screen',
+            arguments=['-d', rviz_file]),
     ])
