14a8cc7f96a73ff08c98d523439a00d18802a218
Lab Computer <pietroluigiruffinomenga@gmail.com>
2025-11-19 13:10:18 +1100

    testing for visualisqaion

--- setupRealur5e.sh
+++ setupRealur5e.sh
@@ -23,21 +23,19 @@ source ~/MTRN4231_Project/install/setup.bash
 
 gnome-terminal -t "DriverServer" -e 'ros2 launch ur_robot_driver ur_control.launch.py ur_type:=ur5e robot_ip:=192.168.0.100 use_fake_hardware:=false launch_rviz:=false'
 
-sleep 10
+sleep 5
 
-gnome-terminal -t "MoveitServer" -e 'ros2 launch ur_moveit_config_custom ur_moveit.launch.py \
-    robot_ip:=192.168.0.100 \
-    ur_type:=ur5e \
-    launch_rviz:=true \
-    use_fake_hardware:=false \
-    description_package:=end_effector_description \
-    description_file:=end_effector_withDriverSupport.xacro'
-
-# gnome-terminal -t "MoveitServer" -e 'ros2 launch ur_moveit_config ur_moveit.launch.py \
+# gnome-terminal -t "MoveitServer" -e 'ros2 launch ur_moveit_config_custom ur_moveit.launch.py \
 #     robot_ip:=192.168.0.100 \
 #     ur_type:=ur5e \
 #     launch_rviz:=true \
-#     description_package:=ur5e_custom_description \
-#     description_file:=ur5e_with_tool.urdf.xacro'
+#     use_fake_hardware:=false \
+#     description_package:=end_effector_description \
+#     description_file:=end_effector_withDriverSupport.xacro'
+
+gnome-terminal -t "MoveitServer" -e 'ros2 launch ur_moveit_config ur_moveit.launch.py \
+    robot_ip:=192.168.0.100 \
+    ur_type:=ur5e \
+    launch_rviz:=true'
 
 gnome-terminal --title="Arm Controller" -- bash -c "ros2 launch arm_controller arm_controller.launch.py execute:=true; exec bash" &
